8110f12d8139249ac2de85894a01e9262a72f30a
AyakaMain <phaomaxe2015@gmail.com>
2023-10-17 15:11:39 -0700

    add demonstration of the game logic

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
 # Coding convention:
 # - in the file or module except `main.py`, prefix everything (classes, fields, etc.)
 # that you don't want others to see with `_` (underscore)
+import dataclasses
 
-from game import Game, Side
+from game import Game, Side, Player
+
+
+@dataclasses.dataclass
+class TestPlayer(Player):
+    current: int
+
+    def decide_move(self, board_view: list[bool | None]) -> int:
+        move = self.current
+        self.current += 1
+        return move
 
 
 def main():
-    g = Game(None, None, Side.X)
-    print(g.next_turn())
+    g = Game(TestPlayer(0), TestPlayer(3), Side.X)
+
+    g.next_turn()
+
+    g.next_turn()
+
+    outcome = g.next_turn()
+    print(outcome)
+
+    g.next_turn()
+
+    outcome = g.next_turn()
+    print(outcome)
 
 
 if __name__ == "__main__":
